24b3b53b91dd59f8babbf8de32fc2cf04a354ae2
Alexey Pashinov <pashinov93@gmail.com>
2022-11-01 18:02:24 +0100

    Improve parsing int msg header

--- src/message.c
+++ src/message.c
@@ -127,6 +127,23 @@ void deserialize_int_message_header(struct SliceData_t* slice, uint8_t flags, Si
     uint8_t amount[amount_length];
     deserialize_amount(slice, amount, amount_length);
     set_amount(amount, amount_length, flags, ctx->decimals, ctx->ticker);
+
+    uint8_t other = SliceData_get_next_bit(slice);
+    UNUSED(other);
+
+    // Fee
+    uint8_t ihr_fee_length = SliceData_get_next_int(slice, 4);
+    VALIDATE(ihr_fee_length == 0, ERR_INVALID_DATA);
+
+    uint8_t fwd_fee_length = SliceData_get_next_int(slice, 4);
+    VALIDATE(fwd_fee_length == 0, ERR_INVALID_DATA);
+
+    // Created
+    uint64_t created_lt = SliceData_get_next_int(slice, 64);
+    UNUSED(created_lt);
+
+    uint32_t created_at = SliceData_get_next_int(slice, 32);
+    UNUSED(created_at);
 }
 
 
